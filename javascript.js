bb43d1ef685e32e3458389d222c8ea899238a4e7
redMonkeh <annasmcclellan@gmail.com>
2024-10-15 21:01:29 -0700

    Create function to create blue squares inside container div.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// create 16x16 grid of square divs with javascript
+// in container div
+
+const container = document.querySelector(".container");
+const containerHeight = container.offsetHeight - 4;
+const containerWidth = container.offsetWidth - 4;
+
+
+function createSquareGrid(row, col){
+    let totalSquares = row * col;
+    let squareHeight = containerHeight/row + "px";
+    let squareWidth = containerWidth/col + "px";
+    
+    for(let i = 0; i < totalSquares; i++){
+        const square = document.createElement("div");
+        square.style.height = squareHeight;
+        square.style.width = squareWidth;
+        square.style.backgroundColor = "blue";
+        container.appendChild(square);
+
+    }
+}
+
+createSquareGrid(16, 16);
