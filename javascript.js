4ffc68424b87d30ca41bd333c04c5bfd8d2460da
redMonkeh <annasmcclellan@gmail.com>
2024-10-16 10:12:49 -0700

    Removed height from container so that any height could occur & squares are always square.

--- javascript.js
+++ javascript.js
@@ -2,27 +2,21 @@
 // in container div
 
 const container = document.querySelector(".container");
-const containerHeight = container.offsetHeight - 4;
 const containerWidth = container.offsetWidth - 4;
 const startButton = document.querySelector("#start");
 
 
 function createSquareGrid(row, col){
     let totalSquares = row * col;
-    let squareHeight = containerHeight/row + "px";
     let squareWidth = containerWidth/col + "px";
     
     for(let i = 0; i < totalSquares; i++){
         const square = document.createElement("div");
-        square.style.height = squareHeight;
         square.style.width = squareWidth;
         square.style.backgroundColor = "lightblue";
         square.addEventListener("mouseover", function () {
             changeSquareColor("pink", square);
         });
-        //square.addEventListener("mouseout", function () {
-        //    changeSquareColor("lightblue", square);
-        //});
         square.addEventListener("mouseout", changeSquareColor("lightblue", square));
         container.appendChild(square);
 
